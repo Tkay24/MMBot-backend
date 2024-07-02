9c016b592f1c163374a7adc271c9f561be8c47a1
Presley <148069174+Tkay24@users.noreply.github.com>
2024-07-02 23:47:22 +0200

    Update catchAsync.js

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,13 @@
 const catchAsync = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  Promise.resolve(fn(req, res, next))
+    .then((result) => {
+       if (result === undefined) return;
+      if (Array.isArray(result) && result.length === 2 && typeof result[0] === 'number' && typeof result[1] === 'object') {
+        const [statusCode, response] = result;
+        return res.status(statusCode).json(response);
+      }
+      res.json(result);
+    })
+    .catch((err) => {
+      next(err); 
 };
-
-module.exports = catchAsync;
