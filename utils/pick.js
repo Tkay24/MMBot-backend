6b38972149a40774b0ba98ff80f53626dc5171db
Presley <148069174+Tkay24@users.noreply.github.com>
2024-07-02 23:50:07 +0200

    Update pick.js

--- utils/pick.js
+++ utils/pick.js
@@ -1,16 +1,20 @@
 /**
  * Create an object composed of the picked object properties
- * @param {Object} object
- * @param {string[]} keys
- * @returns {Object}
+ * @param {Object} object - The source object from which to pick properties
+ * @param {string[]} keys - An array of keys to pick from the source object
+ * @returns {Object} - An object composed of the picked properties
  */
 const pick = (object, keys) => {
+  if (!object || !keys || keys.length === 0) {
+    return {}; // Return an empty object if object is falsy or keys array is empty
+  }
+
   return keys.reduce((obj, key) => {
-    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
-      // eslint-disable-next-line no-param-reassign
-      obj[key] = object[key];
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      // Check if key exists in object before assigning
+      return { ...obj, [key]: object[key] };
     }
-    return obj;
+    return obj; // Return original obj if key does not exist in object
   }, {});
 };
 
