04ad822fe0f9618c79b7b364a6923cbbf4b2a9be
Presley <148069174+Tkay24@users.noreply.github.com>
2024-07-02 22:56:59 +0200

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,25 +1,25 @@
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jsonwebtoken');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { tokenTypes } = require('./token');
 const db = require("../models");
-const User = db.user;
+const User = db.User; 
 
 const jwtOptions = {
-    secretOrKey: 'security' || process.env.JWT_SECRET,
+    secretOrKey: process.env.JWT_SECRET || 'default_secret',
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 };
 
 const jwtVerify = async (payload, done) => {
     try {
         if (payload.type !== tokenTypes.ACCESS) {
-            throw new Error('Invalid token type');
+            return done(null, false, { message: 'Invalid token type' });
         }
         const user = await User.findByPk(payload.sub);
         if (!user) {
-            return done(null, false);
+            return done(null, false, { message: 'User not found' });
         }
-        done(null, user);
+        return done(null, user);
     } catch (error) {
-        done(error, false);
+        return done(error, false);
     }
 };
 
@@ -28,3 +28,4 @@ const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
 module.exports = {
     jwtStrategy,
 };
+
